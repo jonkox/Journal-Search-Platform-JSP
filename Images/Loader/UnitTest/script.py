d1e5237dd243bda7d83314cf389226153047ad66
Johnny Diaz <jhonny4605@estudiantec.cr>
2022-11-23 22:11:48 -0600

    Pruebas unitarias listas

--- Images/Loader/UnitTest/script.py
+++ Images/Loader/UnitTest/script.py
@@ -12,7 +12,7 @@ MARIADBUSER = "root"
 MARIADBPASS = "3VopN5R26q"
 
 def connectMariaDB():
-    val = False
+    val = 2
 
     
     #Connect to mariadb
@@ -28,7 +28,7 @@ def connectMariaDB():
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
 
-    if val:
+    if val == 0:
         jobsTable = "CREATE TABLE IF NOT EXISTS jobs ( \
                             id INT NOT NULL AUTO_INCREMENT, \
                             created DATETIME, \
@@ -53,17 +53,51 @@ def connectMariaDB():
                         )"
         cur.execute("DROP TABLE IF EXISTS my_database.groups")
         cur.execute("DROP TABLE IF EXISTS my_database.jobs")
+
+        conn.commit()
+    
+    if val == 1:
+        cur.execute("select * from jobs")
+        s = cur.fetchall()
+        print(s)
+    
+    if val == 2:
+        cur.execute("DROP DATABASE IF EXISTS LoaderUnitTest")
+        cur.execute("CREATE DATABASE IF NOT EXISTS LoaderUnitTest")
+
+        jobsTable = "CREATE TABLE IF NOT EXISTS LoaderUnitTest.jobs ( \
+                            id INT NOT NULL AUTO_INCREMENT, \
+                            created DATETIME, \
+                            status VARCHAR(45), \
+                            end DATETIME, \
+                            loader VARCHAR(45), \
+                            grp_size INT, \
+                            PRIMARY KEY (id) \
+                        )"
+
+        groupsTable = "CREATE TABLE IF NOT EXISTS LoaderUnitTest.groups ( \
+                            id INT NOT NULL AUTO_INCREMENT, \
+                            id_job INT NOT NULL, \
+                            created DATETIME, \
+                            end DATETIME, \
+                            stage VARCHAR(45), \
+                            grp_number INT, \
+                            status VARCHAR(45), \
+                            `offset` INT, \
+                            PRIMARY KEY (id), \
+                            FOREIGN KEY (id_job) REFERENCES jobs (id) \
+                        )"
+        cur.execute("DROP TABLE IF EXISTS LoaderUnitTest.groups")
+        cur.execute("DROP TABLE IF EXISTS LoaderUnitTest.jobs")
         cur.execute(jobsTable)
         cur.execute(groupsTable)
         conn.commit()
 
-
-        for i in range(200):
+        for i in range(10):
             print("INSERTANDO JOBS")
-            cur.execute(f"insert INTO my_database.jobs(created,status,end,loader,grp_size) values(now(),'NEW',null,null,{str(random.randint(10,2000))})")
+            cur.execute(f"insert INTO LoaderUnitTest.jobs(created,status,end,loader,grp_size) values(now(),'NEW',null,null,{str(random.randint(10,2000))})")
         conn.commit()
-    
-    if not val:
+        cur.execute("USE LoaderUnitTest")
         cur.execute("select * from jobs")
         s = cur.fetchall()
         print(s)
